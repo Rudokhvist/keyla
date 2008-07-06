0c6d69efdce8aa7c05c3b87c975befb704b19c85
earshinov <earshinov@c4dbf6ae-4951-0410-8119-79e3e2651034>
2008-07-06 09:05:47 +0000

    * Added some screen shots * Coded default button in the settings dialog * Tray menu now can't be opened when the settings dialog is active

--- trunk/keyla/settingsWindow.cpp
+++ trunk/keyla/settingsWindow.cpp
@@ -1,6 +1,7 @@
 #include "common.h"
 
 #include "gui/SettingsDialog.h"
+#include "mainWindow.h"
 #include "settingsWindow.h"
 
 static SettingsDialog MySettingsDialog;
@@ -9,6 +10,10 @@ namespace settingsWindow {
 
 	void show() {
 		MySettingsDialog.DoModal();
+	}
+
+	bool isShown() {
+		return IsWindowVisible(MySettingsDialog) != 0;
 	}
 
 }
